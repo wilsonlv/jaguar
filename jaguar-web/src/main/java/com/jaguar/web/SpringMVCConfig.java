79f0ab58a0c843d5f1cf4cacc81228b81d4401a2
lvws <515444876@qq.com>
2019-04-29 17:05:57 +0800

    json序列化long京都丢失bug修复

--- jaguar-web/src/main/java/com/jaguar/web/SpringMVCConfig.java
+++ jaguar-web/src/main/java/com/jaguar/web/SpringMVCConfig.java
@@ -1,11 +1,18 @@
 package com.jaguar.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.jaguar.core.util.DateUtil;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.DateFormatter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * Created by lvws on 2019/4/22.
@@ -20,4 +27,22 @@ public class SpringMVCConfig implements WebMvcConfigurer {
         registry.addFormatter(new DateFormatter(DateUtil.DatePattern.YYYY_MM_DD.toString()));
         registry.addFormatter(new DateFormatter(DateUtil.DatePattern.YYYY_MM_DD_HH_MM_SS.toString()));
     }
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        /**
+         * 序列换成json时,将所有的long变成string
+         * 因为js中得数字类型不能包含所有的java long值
+         */
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(simpleModule);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+    }
+
 }
