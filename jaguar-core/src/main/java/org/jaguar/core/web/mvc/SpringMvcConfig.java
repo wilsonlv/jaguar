96f961a627ddf867b33926a29f989e371bafa5ef
515444876@qq.com <515444876@qq.com>
2020-06-11 16:00:17 +0800

    SpringMvcConfig string->local

--- jaguar-core/src/main/java/org/jaguar/core/web/mvc/SpringMvcConfig.java
+++ jaguar-core/src/main/java/org/jaguar/core/web/mvc/SpringMvcConfig.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.apache.commons.lang3.StringUtils;
 import org.jaguar.commons.utils.DateUtil;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
@@ -36,6 +38,46 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         .deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.YYYY_MM_DD.toString())))
                         .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.YYYY_MM_DD_HH_MM_SS.toString())))
                         .deserializerByType(LocalTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.HH_MM_SS.toString())));
+    }
+
+    @Bean
+    public Converter<String, LocalTime> localTimeConverter() {
+        return new Converter<String, LocalTime>() {
+            @Override
+            public LocalTime convert(String source) {
+                if (StringUtils.isBlank(source)) {
+                    return null;
+                }
+                return LocalTime.parse(source, DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.HH_MM_SS.toString()));
+            }
+        };
+    }
+
+    @Bean
+    public Converter<String, LocalDate> localDateConverter() {
+        return new Converter<String, LocalDate>() {
+
+            @Override
+            public LocalDate convert(String source) {
+                if (StringUtils.isBlank(source)) {
+                    return null;
+                }
+                return LocalDate.parse(source, DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.YYYY_MM_DD.toString()));
+            }
+        };
+    }
+
+    @Bean
+    public Converter<String, LocalDateTime> localDateTimeConverter() {
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(String source) {
+                if (StringUtils.isBlank(source)) {
+                    return null;
+                }
+                return LocalDateTime.parse(source, DateTimeFormatter.ofPattern(DateUtil.DateTimePattern.YYYY_MM_DD_HH_MM_SS.toString()));
+            }
+        };
     }
 
 }
