26200af69d5a6d3d914c6bf6e84b3e9ac7a912e1
515444876@qq.com <515444876@qq.com>
2020-10-15 15:54:17 +0800

    查询总行数

--- jaguar-core/src/main/java/org/jaguar/core/base/BaseMapper.java
+++ jaguar-core/src/main/java/org/jaguar/core/base/BaseMapper.java
@@ -1,9 +1,22 @@
 package org.jaguar.core.base;
 
+import org.apache.ibatis.annotations.Select;
+
 /**
  * @author lvws
  * @since 2019/5/6.
  */
 public interface BaseMapper<T extends BaseModel> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
+
+    /**
+     * 查询上一条sql的总行数
+     *
+     * 注：1、要开启事务
+     *    2、查询sql中需要加上 SQL_CALC_FOUND_ROWS
+     *
+     * @return 总行数
+     */
+    @Select("SELECT found_rows()")
+    int queryTotal();
 
 }
