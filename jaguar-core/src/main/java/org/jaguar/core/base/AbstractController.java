c129873feccde07af90b4ab43b30d38e2b0dc70d
lvws <515444876@qq.com>
2021-03-30 19:14:07 +0800

    重构

--- jaguar-core/src/main/java/org/jaguar/core/base/AbstractController.java
+++ jaguar-core/src/main/java/org/jaguar/core/base/AbstractController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.jaguar.core.web.JsonResult;
 import org.jaguar.core.web.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -19,42 +18,42 @@ public abstract class AbstractController<T extends BaseModel,
     @Autowired
     protected N service;
 
-    public ResponseEntity<JsonResult<Page<T>>> query(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page) {
+    public JsonResult<Page<T>> query(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page) {
         return this.query(page, Wrappers.emptyWrapper());
     }
 
-    public ResponseEntity<JsonResult<Page<T>>> query(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page,
-                                                     Wrapper<T> queryWrapper) {
+    public JsonResult<Page<T>> query(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page,
+                                     Wrapper<T> queryWrapper) {
         page = service.query(page, queryWrapper);
         return success(page);
     }
 
-    public ResponseEntity<JsonResult<List<T>>> list() {
+    public JsonResult<List<T>> list() {
         List<T> list = service.list();
         return success(list);
     }
 
-    public ResponseEntity<JsonResult<T>> getById(Long id) {
+    public JsonResult<T> getById(Long id) {
         T model = service.getById(id);
         return success(model);
     }
 
-    public ResponseEntity<JsonResult<T>> insert(T entity) {
+    public JsonResult<T> insert(T entity) {
         T model = service.insert(entity);
         return success(model);
     }
 
-    public ResponseEntity<JsonResult<T>> updateById(T entity) {
+    public JsonResult<T> updateById(T entity) {
         T model = service.updateById(entity);
         return success(model);
     }
 
-    public ResponseEntity<JsonResult<T>> saveOrUpdate(T entity) {
+    public JsonResult<T> saveOrUpdate(T entity) {
         T model = service.saveOrUpdate(entity);
         return success(model);
     }
 
-    public ResponseEntity<JsonResult<?>> delete(Long id) {
+    public JsonResult<?> delete(Long id) {
         service.delete(id);
         return success();
     }
