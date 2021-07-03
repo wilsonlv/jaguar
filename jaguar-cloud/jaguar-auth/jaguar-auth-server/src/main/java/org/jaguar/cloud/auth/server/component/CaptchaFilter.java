0411ad4f83a3cbb52e83e479be9a5d62d8283d4c
wilson <515444876@qq.com>
2021-07-03 15:32:36 +0800

    CaptchaFilter 优化

--- jaguar-cloud/jaguar-auth/jaguar-auth-server/src/main/java/org/jaguar/cloud/auth/server/component/CaptchaFilter.java
+++ jaguar-cloud/jaguar-auth/jaguar-auth-server/src/main/java/org/jaguar/cloud/auth/server/component/CaptchaFilter.java
@@ -1,6 +1,5 @@
 package org.jaguar.cloud.auth.server.component;
 
-import com.anji.captcha.model.common.CaptchaTypeEnum;
 import com.anji.captcha.model.common.ResponseModel;
 import com.anji.captcha.model.vo.CaptchaVO;
 import com.anji.captcha.service.CaptchaService;
@@ -8,6 +7,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.jaguar.commons.web.JsonResult;
 import org.jaguar.commons.web.ResultCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author lvws
@@ -40,14 +41,15 @@ public class CaptchaFilter extends OncePerRequestFilter {
         }
 
         CaptchaVO captcha = new CaptchaVO();
-        captcha.setPointJson(request.getParameter("pointJson"));
-        captcha.setToken(request.getParameter("token"));
-        captcha.setCaptchaType(CaptchaTypeEnum.BLOCKPUZZLE.getCodeValue());
-        ResponseModel responseModel = captchaService.check(captcha);
+        captcha.setCaptchaVerification(request.getParameter("captchaVerification"));
+        ResponseModel responseModel = captchaService.verification(captcha);
 
         if (responseModel.isSuccess()) {
             filterChain.doFilter(request, response);
         } else {
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            response.setContentType(MediaType.APPLICATION_JSON.toString());
+
             try (PrintWriter writer = response.getWriter()) {
                 JsonResult<ResponseModel> result = new JsonResult<>(ResultCode.CONFLICT, responseModel, responseModel.getRepMsg());
                 writer.write(result.toJsonStr());
