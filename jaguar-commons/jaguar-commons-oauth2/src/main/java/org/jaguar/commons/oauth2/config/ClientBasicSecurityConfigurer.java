f2de71eb47df6d6eeb5d13d39fb8f53860f2f4e0
wilson <515444876@qq.com>
2021-07-05 14:10:56 +0800

    ClientBasicAuthServiceImpl

--- jaguar-commons/jaguar-commons-oauth2/src/main/java/org/jaguar/commons/oauth2/config/ClientBasicSecurityConfigurer.java
+++ jaguar-commons/jaguar-commons-oauth2/src/main/java/org/jaguar/commons/oauth2/config/ClientBasicSecurityConfigurer.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.jaguar.commons.oauth2.component.AuthenticationExceptionHandler;
 import org.jaguar.commons.oauth2.component.ClientBasicAuthServiceImpl;
 import org.jaguar.commons.oauth2.component.JaguarAccessDeniedHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,19 +17,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class ClientBasicSecurityConfigurer {
 
-    private static JaguarAccessDeniedHandler JAGUAR_ACCESS_DENIED_HANDLER;
-
-    private static AuthenticationExceptionHandler AUTHENTICATION_EXCEPTION_HANDLER;
-
-
-    protected static void configureClientBasic(HttpSecurity http, JaguarAccessDeniedHandler accessDeniedHandler,
-                                               AuthenticationExceptionHandler exceptionHandler) throws Exception {
+    protected static void configureClientBasic(HttpSecurity http, ApplicationContext applicationContext) throws Exception {
 
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler)
-                .and().httpBasic().authenticationEntryPoint(exceptionHandler);
+                .and().exceptionHandling().accessDeniedHandler(applicationContext.getBean(JaguarAccessDeniedHandler.class))
+                .and().httpBasic().authenticationEntryPoint(applicationContext.getBean(AuthenticationExceptionHandler.class));
     }
 
     @Order(1)
@@ -37,21 +31,13 @@ public class ClientBasicSecurityConfigurer {
     @RequiredArgsConstructor
     static class ActuatorSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-        @Autowired
-        public void setJaguarAccessDeniedHandler(JaguarAccessDeniedHandler accessDeniedHandler) {
-            JAGUAR_ACCESS_DENIED_HANDLER = accessDeniedHandler;
-        }
-
-        @Autowired
-        public void setAuthenticationExceptionHandler(AuthenticationExceptionHandler authenticationExceptionHandler) {
-            AUTHENTICATION_EXCEPTION_HANDLER = authenticationExceptionHandler;
-        }
+        private final ApplicationContext applicationContext;
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.antMatcher("/actuator").antMatcher("/actuator/**")
                     .userDetailsService(new ClientBasicAuthServiceImpl("actuator"));
-            configureClientBasic(http, JAGUAR_ACCESS_DENIED_HANDLER, AUTHENTICATION_EXCEPTION_HANDLER);
+            configureClientBasic(http, applicationContext);
         }
     }
 
@@ -60,11 +46,13 @@ public class ClientBasicSecurityConfigurer {
     @RequiredArgsConstructor
     static class FeignSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+        private final ApplicationContext applicationContext;
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.antMatcher("/feign/**")
                     .userDetailsService(new ClientBasicAuthServiceImpl("feign"));
-            configureClientBasic(http, JAGUAR_ACCESS_DENIED_HANDLER, AUTHENTICATION_EXCEPTION_HANDLER);
+            configureClientBasic(http, applicationContext);
         }
     }
 
