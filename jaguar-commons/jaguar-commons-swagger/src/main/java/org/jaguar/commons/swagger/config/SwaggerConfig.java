f3d67d91e50fbae9ee7e57015ad936b64ff47c11
wilson <515444876@qq.com>
2021-07-26 09:55:00 +0800

    swagger

--- jaguar-commons/jaguar-commons-swagger/src/main/java/org/jaguar/commons/swagger/config/SwaggerConfig.java
+++ jaguar-commons/jaguar-commons-swagger/src/main/java/org/jaguar/commons/swagger/config/SwaggerConfig.java
@@ -2,14 +2,22 @@ package org.jaguar.commons.swagger.config;
 
 import org.jaguar.commons.swagger.properties.SwaggerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lws
@@ -21,10 +29,18 @@ public class SwaggerConfig {
 
     @Autowired
     private SwaggerProperties swaggerProperties;
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @Bean
     public Docket platformApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("full-platform").apiInfo(apiInfo())
+        List<RequestParameter> parameters = new ArrayList<>();
+        RequestParameter build = new RequestParameterBuilder().name("Authorization")
+                .in(ParameterType.HEADER).required(false).build();
+        parameters.add(build);
+
+        return new Docket(DocumentationType.SWAGGER_2).groupName(applicationName).apiInfo(apiInfo())
+                .globalRequestParameters(parameters)
                 .forCodeGeneration(true);
     }
 
