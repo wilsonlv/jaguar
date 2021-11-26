b4dd48de0736c969ee90e04dea7bf8a1b6ff72de
wilsonlv <515444876@qq.com>
2021-11-26 13:34:50 +0800

    Autowired -> Resource

--- jaguar-support/jaguar-data-modify-log/src/main/java/top/wilsonlv/jaguar/support/datamodifylog/aop/DataModifyLogAspect.java
+++ jaguar-support/jaguar-data-modify-log/src/main/java/top/wilsonlv/jaguar/support/datamodifylog/aop/DataModifyLogAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
